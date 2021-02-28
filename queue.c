2aac39e2e0ff8df5cf94129bca164305a2061153
Howard <s880367@gmail.com>
2021-02-28 02:00:04 -0800

    Update sort
    Not need to start from head for finding tail

--- queue.c
+++ queue.c
@@ -222,7 +222,7 @@ list_ele_t *merge(list_ele_t *l1, list_ele_t *l2)
     if (!l1)
         return l2;
 
-    if (strcasecmp(l1->value, l2->value) <= 0) {
+    if (strcasecmp(l1->value, l2->value) < 0) {
         l1->next = merge(l1->next, l2);
         return l1;
     } else {
@@ -257,16 +257,13 @@ list_ele_t *mergeSortList(list_ele_t *head)
 
 void q_sort(queue_t *q)
 {
-    list_ele_t *node;
-
     if (!q || !q->head)
         return;
 
     q->head = mergeSortList(q->head);
 
-    node = q->head;
-    while (node->next) {
-        node = node->next;
+    q->tail = q->head;
+    while (q->tail->next) {
+        q->tail = q->tail->next;
     }
-    q->tail = node;
 }
