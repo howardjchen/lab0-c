1e80a1ca0b83d5f5a33792a9a3052b298838f930
Howard Chen <s880367@gmail.com>
2024-02-28 01:06:53 +0800

    Fix buffer size less than copy length
    When bufsize is less than the copy length of string
    Need to copy only length of (bufsize - 1) and tail a '\0'
    in the end of the sp

--- queue.c
+++ queue.c
@@ -85,10 +85,12 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 
     ele = list_first_entry(head, element_t, list);
     if (sp) {
-        if (strlen(ele->value) < bufsize)
+        if (strlen(ele->value) <= bufsize)
             memcpy(sp, ele->value, strlen(ele->value) + 1);
-        else
-            memcpy(sp, ele->value, bufsize);
+        else {
+            memcpy(sp, ele->value, bufsize - 1);
+            sp[bufsize - 1] = '\0';
+        }
     }
 
     list_del(head->next);
@@ -106,10 +108,12 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 
     ele = list_last_entry(head, element_t, list);
     if (sp) {
-        if (strlen(ele->value) < bufsize)
+        if (strlen(ele->value) <= bufsize)
             memcpy(sp, ele->value, strlen(ele->value) + 1);
-        else
-            memcpy(sp, ele->value, bufsize);
+        else {
+            memcpy(sp, ele->value, bufsize - 1);
+            sp[bufsize - 1] = '\0';
+        }
     }
 
     list_del(head->next);
