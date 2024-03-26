f95866cf7347f6fda9735187132ebebd70624497
Howard Chen <s880367@gmail.com>
2024-03-26 16:46:35 +0800

    Disable Ctrl-S and Ctrl-Q
    By default, Ctrl-S and Ctrl-Q are used for software flow control.
    Ctrl-S stops data from being transmitted to the terminal until
    you press Ctrl-Q. This originates in the days when you might want to
    pause the transmission of data to let a device like a printer catch up.
    Need to turn this feature off in order to detect keyboard event Ctrl-Q

--- ttt/ttt.c
+++ ttt/ttt.c
@@ -221,7 +221,7 @@ void enable_raw_mode()
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disable_raw_mode);
     struct termios raw = orig_termios;
-    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
